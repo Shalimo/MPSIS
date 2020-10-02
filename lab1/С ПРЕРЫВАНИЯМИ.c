7bafa5a3d1e514fdbd667a27ffbc9e07be1d5559
Nikita Shalimo 750503 <45921781+Shalimo@users.noreply.github.com>
2020-10-02 15:09:18 +0300

    Update С ПРЕРЫВАНИЯМИ.c

--- lab1/С ПРЕРЫВАНИЯМИ.c
+++ lab1/С ПРЕРЫВАНИЯМИ.c
@@ -8,33 +8,33 @@ int button2();
  */
 int main(void)
 {
-  WDTCTL = WDTPW | WDTHOLD;	// off timer of real time
+  WDTCTL = WDTPW | WDTHOLD;	
 
   //LED
-  P1DIR |= BIT4; // установка направления ножки P1.4 на выход
-  P1SEL &= ~BIT4; // установка использования ножки P1.4 в качестве порта ввода-вывода
-  P1OUT &= ~BIT4; // установка значения 0 на вывод ножки P1.4
+  P1DIR |= BIT4; 
+  P1SEL &= ~BIT4; 
+  P1OUT &= ~BIT4; 
   //S1
-  P1DIR  &= ~BIT7; // установка направления ножки P1.7 на вход
-  P1REN |= BIT7; // включение подтягивающего резистора ножки P1.7
-  P1OUT |= BIT7; // установка значения 0 на вывод ножки P1.7
-  P1SEL &= ~BIT7; // установка использования ножки P1.7 в качестве порта ввода-вывода
+  P1DIR  &= ~BIT7; 
+  P1REN |= BIT7; 
+  P1OUT |= BIT7; 
+  P1SEL &= ~BIT7;
   //S2
-  P2DIR  &= ~BIT2; // установка направления ножки P2.2 на вход
-  P2REN |= BIT2; // включение подтягивающего резистора ножки P2.2
-  P2OUT |= BIT2; // установка значения 0 на вывод ножки P2.2
-  P2SEL &= ~BIT2; // установка использования ножки P2.2 в качестве порта ввода-вывода
+  P2DIR  &= ~BIT2; 
+  P2REN |= BIT2; 
+  P2OUT |= BIT2; 
+  P2SEL &= ~BIT2; 
 
-  _disable_interrupts(); //запрет на запрос на прерывание
-  P2IE |= BIT2; // разрешение прерывания по линии ввода-вывода P2.2
-  P2IES &= ~BIT2; // выбор генерации запроса на прерывание по фронту по линии ввода-вывода P2.2
-  P2IFG &= ~BIT2; // установка флага запроса на прерывание по линии ввода-вывода P2.2 в 0
-  _enable_interrupts(); // разрешение на запрос на прерывание
+  _disable_interrupts(); 
+  P2IE |= BIT2; 
+  P2IES &= ~BIT2; 
+  P2IFG &= ~BIT2; 
+  _enable_interrupts();
 
-    __no_operation(); //бесконечный цикл
+    __no_operation(); 
 }
-#pragma vector = PORT2_VECTOR //директива для замены вектора прерываний
-__interrupt void ISR_S2() //обработчик прерываний для S2
+#pragma vector = PORT2_VECTOR 
+__interrupt void ISR_S2() 
 {
 	delay(1000);
 
@@ -42,11 +42,11 @@ __interrupt void ISR_S2() //обработчик прерываний для S2
 	{
 		if(button1())
 		{
-			P1OUT ^= BIT4; // записываем в бит что отвечает за диод, в регистре out значение тем самым инвертируем прошлое состояние
+			P1OUT ^= BIT4; 
 			delay(1000);
 		}
 	}
-	P2IFG &= ~BIT2; //убираем флаг прерываний для S2
+	P2IFG &= ~BIT2; 
     return;
 }
 
@@ -54,7 +54,7 @@ int button1()
 {
 	return (P1IN & BIT7) ? 0 : 1;
 }
-// 0 - off / 1 - on
+
 int button2()
 {
 	return (P2IN & BIT2) ? 0 : 1;
